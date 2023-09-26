56757848847a02099a93011ccdc7bc6f623be6f9
zephyr steiner <esteiner@smith.edu>
2023-09-26 15:43:01 -0400

    modified:   ConversationIntro.java new file:   lib/hamcrest-core-1.3.jar new file:   lib/junit-4.13.2.jar

--- ConversationIntro.java
+++ ConversationIntro.java
@@ -9,7 +9,7 @@ public class ConversationIntro {
   
   /** Returns an integer one larger than its input */
   public int plusOne(int n) {
-    // n = n+1;
+    n = n+1;
     return n;
   }
 
@@ -21,7 +21,8 @@ public class ConversationIntro {
   
   /** Returns an integer that is double its input */
   public int timesTwo(int n) {
-    return 0;
+    n=n*2;
+    return n;
   }
 
   /*
@@ -30,7 +31,8 @@ public class ConversationIntro {
   
   /** Returns the square of its input */
   public int squared(int n) {
-    return 0;
+    n = (int) Math.pow(n,2);
+    return n;
   }
 
   /*
@@ -39,7 +41,8 @@ public class ConversationIntro {
   
   /** Divides its input by seven, dropping remainders */
   public int intDivideBy7(int n) {
-    return 0;
+    n = (int) Math.floorDiv(n,7);
+    return n;
   }
 
   /*
@@ -48,7 +51,9 @@ public class ConversationIntro {
   
   /** Divides its input by 7 */
   public double doubleDivideBy7(int n) {
-    return 0;
+    double d = n;
+    d = d/7;
+    return d;
   }
 
   /*
@@ -63,11 +68,11 @@ public class ConversationIntro {
   /** Returns true if its input is prime */
   public static boolean isPrime(int n) {
     boolean isPrime = (n > 1); // one, zero, and negatives not prime
-    /*for (int i = 2; i <= Math.sqrt(n); i++) {
+    for (int i = 2; i <= Math.sqrt(n); i++) {
         if (n%i == 0) {
             isPrime = false;
         }
-    }*/
+    }
     return isPrime;
   }
 
@@ -77,7 +82,15 @@ public class ConversationIntro {
   
   /** Returns true if its input is even */
   public boolean isEven(int n) {
-    return false;
+    boolean isEven = (n%2 == 0);
+    // if (n%2 != 0) {
+    //     boolean isEven = false;
+    // }
+    // else { 
+    //     boolean isEven = true;
+    // }
+    
+    return isEven;
   }
 
   /*
@@ -92,8 +105,8 @@ public class ConversationIntro {
   
   /** Returns true if its input is a comma */
   public static boolean isComma(String s) {
-    //return s.equals(",");
-    return (s == ",");
+    return s.equals(",");
+    // return (s == ",");
   }
 
   /*
@@ -102,7 +115,12 @@ public class ConversationIntro {
   
   /** Returns true if its input is one of:  .,?! */
   public boolean isPunctuation(String s) {
-    return false;
+    boolean isPunctuation = false;
+    if (s.equals(".") || s.equals(",")
+    || s.equals("?")|| s.equals("!")) {
+      isPunctuation = true;
+    }
+    return isPunctuation;
   }
 
   /*
@@ -110,8 +128,23 @@ public class ConversationIntro {
    */
   
   /** Returns its input, unless it is "I" or "you", which switch */
+  // do things word by word - String[] words = u_i.split(" ")
   public String mirrorWord(String s) {
-    return s;
+    String[] words = s.split(" ");
+    for (int i = 0; i<words.length; i++) {
+      if (words[i] == "I") {
+        // element = "you";
+        // element.replace("I", "you");
+        words[i] = "you";
+      }
+      else if (words[i] == "you") {
+        // element = "I";
+        // element.replace("you", "I");
+        words[i] = "I";
+      }
+    }
+    String words_out = String.join(" ", words);
+    return words_out;
   }
 
   /*
@@ -125,9 +158,9 @@ public class ConversationIntro {
   /** Returns an array of whole numbers up to n */
   public static int[] wholeNumbers(int n) {
     int[] arr = new int[n]; // allocate space for the array
-    //for (int i = 0; i < n; i++) {
-    //  arr[i] = i+1;
-    //}
+    for (int i = 0; i < n; i++) {
+     arr[i] = i+1;
+    }
     return arr;
   }
 
@@ -139,6 +172,11 @@ public class ConversationIntro {
   /** Returns an array of squares from 1 to n*n */
   public static int[] squares(int n) {
     int[] arr = new int[n]; // allocate space for the array
+    for (int i =0; i < n; i++) {
+      
+      arr[i] = (int) Math.pow(i,2);
+    }
+    
     return arr;
   }
 }
